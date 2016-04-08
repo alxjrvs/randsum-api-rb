bb1589097e99085fa4a004c2dddc6022a672e922
Alex Jarvis <alxjrvs@gmail.com>
2016-04-08 07:35:20 -0400

    initial commit

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source 'https://rubygems.org'
+
+gem 'sinatra'
+gem 'rollr'
+
+gem 'pry-remote', group: [:test, :development]
+
+gem 'codeclimate-test-reporter', group: :test, require: nil
---
+++ server.rb
@@ -0,0 +1,51 @@
+require 'sinatra'
+require 'rollr'
+require 'json'
+require 'pry'
+
+
+get '/' do
+  return "/roll/1/d/20, or just /roll."
+end
+
+get '/roll' do
+  content_type :json
+
+  result = Rollr::D20.roll
+  serialize result
+end
+
+get '/roll/:num/d/:sides' do
+  content_type :json
+
+  if bad params
+    404
+  else
+    sides = params[:sides].to_i
+    num = params[:num].to_i
+
+    die = Rollr::Die.new(sides.to_i)
+    result = die.roll(number: num.to_i)
+    
+    serialize result
+  end
+
+end
+
+error 404 do
+  'Bad parameters'
+end
+
+def bad(params)
+  return true if params[:sides].to_i == 0
+  return true if params[:num].to_i == 0
+  return false
+end
+def serialize(result)
+  {
+    total:          result.total,
+    rolls:          result.rolls,
+    numer_of_dice:  result.number_of_dice,
+    die_sides:      result.die_sides
+  }.to_json
+end
